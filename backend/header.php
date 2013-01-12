2d0f9ddc9aeb611e7376e1d9716c2a54853311ae
Patrick Konstandin <support@flatfiler.de>
2013-01-12 20:41:56 +0100

    add mootools datepicker
    Signed-off-by: Patrick Konstandin <support@flatfiler.de>

--- backend/header.php
+++ backend/header.php
@@ -1,8 +1,8 @@
 <?php
-/*
-@modul flatCal
-header file
-*/
+/**
+ * @modul flatCal
+ * header file
+ */
 
 if(!defined('FC_INC_DIR')) {
 	die("No access");
@@ -10,4 +10,20 @@ if(!defined('FC_INC_DIR')) {
 
 ?>
 
-<link rel="stylesheet" href="../modules/flatCal.mod/styles/styles.css" type="text/css" media="screen, projection">
+<link rel="stylesheet" href="../modules/flatCal.mod/styles/acp.css" type="text/css" media="screen, projection">
+
+<script type="text/javascript" src="../modules/flatCal.mod/js/datepicker.js"></script>
+
+<script type="text/javascript">
+/* <![CDATA[ */
+
+window.addEvent('load', function() {
+	new DatePicker('.dp', { months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+	days: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+	format: 'd.m.Y',
+	allowEmpty: true,
+	positionOffset: { x: 0, y: 5 }});
+});
+
+/* ]]> */
+</script>
